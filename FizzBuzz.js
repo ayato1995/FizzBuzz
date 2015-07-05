6502ab3bc27ee6ed28f9c94df2cab3d358ccae2f
ayato1995 <ayato.k070206@outlook.com>
2015-07-05 23:33:27 +0900

    FizzBuzz create

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+function FizzBuzz() {
+  for (var i = 1; i <= 50; i++) {
+    if (i % 15 == 0){
+      console.log(i + " FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log(i + " Fizz");
+    } else if (i % 5 == 0) {
+      console.log(i + " Buzz")
+    } else {
+      console.log(i);
+    }
+  }
+}
